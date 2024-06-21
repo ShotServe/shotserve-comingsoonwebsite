033c298971a844c253e5446b90729eafd5592591
Rahul Gupta <112651541+Rrocketter@users.noreply.github.com>
2024-06-21 10:14:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shotserve-comingsoonwebsite.github.io',
+  site: 'https://shotserve.github.io/shotserve-comingsoonwebsite',
   base: 'https://github.com/ShotServe/shotserve-comingsoonwebsite',
 })
